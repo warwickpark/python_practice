3ed2968a8da3674cbbfff1903d3bfecb64db09a6
warwickpark <pjw7750@gmail.com>
2021-08-19 23:20:06 +0900

    21.08.19

--- python-restart.py
+++ python-restart.py
@@ -1,11 +1,19 @@
 from tkinter import *
+#원래는 import 안쓰고 tkinter.Tk() 같이 써야함
 
 root = Tk()
-root.title("TEST")
-root.geometry("640x480")
+root.title("와! GUI!")
+root.geometry("320x240")
 #창의 속성 지정
 
-btn1=Button(root, text="버튼!!")
+btn1=Button(root,activebackground="#16AC81", text="와! 꺼무위키색!")
+btn2=Button(root,activebackground="#1DA1F2", text="누르면 트위터 색!")
+label1=Label(root, text="라벨?",anchor="s", fg="blue", bitmap="error")
+label2=Label(root,text="경고!",anchor="center", width="1",height="1", relief="flat")
+
+label1.pack()
+label2.pack()
 btn1.pack()
+btn2.pack()
 
 root.mainloop()
